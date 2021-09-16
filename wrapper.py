8f8f17b30bc9924e1eaf970db6365428e0373366
Zachary Yocum <zyocum@basistech.com>
2021-09-16 14:16:50 -0400

    better handling of multi-value parameters

--- wrapper.py
+++ wrapper.py
@@ -10,6 +10,17 @@ import requests
 
 from urllib import parse
 
+def sep_join(values):
+    """Prepare parameters that take multiple values
+    See https://www.wikidata.org/w/api.php?action=help&modules=main#main/datatypes
+    """
+    if any((('|' in set(v)) for v in values)):
+        # we need to use an INFORMATION SEPARATOR ONE (U+001F) character as delimiter
+        SEPARATOR = '\u001F'
+        return SEPARATOR + SEPARATOR.join(values)
+    else:
+        return '|'.join(values)
+
 def wbgetentities(
     *ids,
     sites=['wikidatawiki'],
@@ -34,22 +45,22 @@ def wbgetentities(
         raise ValueError('expected at least one ID')
     query = {
         'action': 'wbgetentities',
-        'ids': '|'.join(ids),
+        'ids': sep_join(ids),
         'redirects': redirects,
         'format': 'json'
     }
     if any(sites):
-        query['sites'] = '|'.join(sites)
+        query['sites'] = sep_join(sites)
     if any(props):
-        query['props'] = '|'.join(props)
+        query['props'] = sep_join(props)
     if any(languages):
-        query['languages'] = '|'.join(languages)
+        query['languages'] = sep_join(languages)
     if languagefallback:
         query['languagefallback'] = True
     if normalize:
         query['normalize'] = True
     if any(sitefilter):
-        query['sitefilter'] = '|'.join(sitefilter)
+        query['sitefilter'] = sep_join(sitefilter)
     url = parse.urlunparse(
         parse.ParseResult(
             scheme='https',
@@ -90,7 +101,7 @@ def wbsearchentities(
     if strictlanguage:
         query['strictlanguage'] = True
     if any(props):
-        query['props'] = '|'.join(props)
+        query['props'] = sep_join(props)
     url = parse.urlunparse(
         parse.ParseResult(
             scheme='https',
